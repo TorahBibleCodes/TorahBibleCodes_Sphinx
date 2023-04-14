ae616cba0d21d18c29f9fc1a0f20b96451296e6d
TorahBibleCodes.com <33508112+TorahBibleCodes@users.noreply.github.com>
2023-04-14 12:03:31 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TorahBibleCodes'
+copyright = '2017-2023, TorahBibleCodes'
+author = 'TorahBibleCodes'
 
 release = '0.1'
 version = '0.1.0'
